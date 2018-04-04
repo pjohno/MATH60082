053e081d841abb649ed0b1016e451635ddc55af9
Paul Johnson <paul.johnson-2@manchester.ac.uk>
2018-04-04 15:48:29 +0100

    included the American option

--- include/math60082_cn.hpp
+++ include/math60082_cn.hpp
@@ -6,24 +6,48 @@
 
 namespace MATH60082
 {
+/** 
+@brief Return the value of a European Put option using the Crank-Nicolson method with SOR solver
+ON INPUT:
+@param S0          -- initial stock price
+@param X           -- exercise (strike) price
+@param T           -- Time to expiry (years)
+@param r           -- interest rate (per annum)
+@param sigma       -- volatility (per annum^12)
+@param iMax        -- number of time steps
+@param jMax        -- number of space steps
+@param SMax        -- Maximum value of S
+@param omega       -- is the relaxation parameter 
+@param tol         -- is the tolerance level
+@param iterMax     -- is maximum iterations
+ON OUTPUT:
+@return the value of a European put option at \f$ S=S0 \f$, \f$ t=0 \f$
+**/
+double europeanPut_CN(double S0,double X,double T,double r,double sigma,int iMax,int jMax,double SMax,double omega,double tol,int iterMax);
+
+/** 
+@brief Return the value of an American Put option using the Crank-Nicolson method with PSOR solver
+ON INPUT:
+@param S0          -- initial stock price
+@param X           -- exercise (strike) price
+@param T           -- Time to expiry (years)
+@param r           -- interest rate (per annum)
+@param sigma       -- volatility (per annum^12)
+@param iMax        -- number of time steps
+@param jMax        -- number of space steps
+@param SMax        -- Maximum value of S
+@param omega       -- is the relaxation parameter 
+@param tol         -- is the tolerance level
+@param iterMax     -- is maximum iterations
+ON OUTPUT:
+@return the value of a American put option at \f$ S=S0 \f$, \f$ t=0 \f$
+**/
+double americanPut_CN(double S0,double X,double T,double r,double sigma,int iMax,int jMax,double SMax,double omega,double tol,int iterMax);
+
 /* 
 ON INPUT:
 S0          -- initial stock price
-X           -- exercise (strike) price
-T           -- Time to expiry (years)
-r           -- interest rate (per annum)
-sigma       -- volatility (per annum^12)
-iMax        -- number of time steps
-jMax        -- number of space steps
-SMax        -- Maximum value of S
-omega       -- is the relaxation parameter 
-tol         -- is the tolerance level
-iterMax     -- is maximum iterations
-ON OUTPUT:
-return      -- the value of a European put option at S=S0, t=0
 */
-double europeanPut_CN(double S0,double X,double T,double r,double sigma,int iMax,int jMax,double SMax,double omega,double tol,int iterMax);
-
 double normalDistribution(double x);
 
 // return the value of a put option using the black scholes formula
--- src/math60082_cn.cpp
+++ src/math60082_cn.cpp
@@ -2,22 +2,7 @@
 
 namespace MATH60082
 {
-/* 
-ON INPUT:
-S0          -- initial stock price
-X           -- exercise (strike) price
-T           -- Time to expiry (years)
-r           -- interest rate (per annum)
-sigma       -- volatility (per annum^12)
-iMax        -- number of time steps
-jMax        -- number of space steps
-SMax        -- Maximum value of S
-omega       -- is the relaxation parameter 
-tol         -- is the tolerance level
-iterMax     -- is maximum iterations
-ON OUTPUT:
-return      -- the value of a European put option at S=S0, t=0
-*/
+
 double europeanPut_CN(double S0,double X,double T,double r,double sigma,int iMax,int jMax,double SMax,double omega,double tol,int iterMax)
 {
     double dS=SMax/jMax;
@@ -95,6 +80,89 @@ double europeanPut_CN(double S0,double X,double T,double r,double sigma,int iMax
     sum+=(S[jStar+1] - S0)/dS * vNew[jStar];
     return sum;
 }
+
+
+double americanPut_CN(double S0,double X,double T,double r,double sigma,int iMax,int jMax,double SMax,double omega,double tol,int iterMax)
+{
+    double dS=SMax/jMax;
+    double dt=T/iMax;
+    // create storage for the stock price and option price (old and new)
+    std::vector<double> S(jMax+1),vOld(jMax+1),vNew(jMax+1);
+    // setup and initialise the stock price 
+    for(int j=0;j<=jMax;j++)
+    {
+        S[j] = j*dS;
+    }
+    // reset initial condition
+    for(int j=0;j<=jMax;j++)
+    {
+        vOld[j] = std::max(X-S[j],0.);
+        vNew[j] = std::max(X-S[j],0.);
+    }
+    // run through timesteps
+    for(int i=iMax-1;i>=0;i--)
+    {
+        // declare vectors for matrix equations
+        std::vector<double> a(jMax+1),b(jMax+1),c(jMax+1),d(jMax+1);
+        // set up matrix equations a[j]=
+        a[0] = 0.;b[0] = 1.;c[0] = 0.;
+        d[0] = X*exp(-r*(iMax-i)*dt);
+        for(int j=1;j<jMax;j++)
+        {
+            a[j] = 0.25*(sigma*sigma*j*j-r*j);
+            b[j] =-0.5*sigma*sigma*j*j - 0.5*r - 1./dt;
+            c[j] = 0.25*(sigma*sigma*j*j+r*j);
+            d[j] =-a[j]*vOld[j-1] - (b[j]+2./dt)*vOld[j] - c[j]*vOld[j+1];
+        }
+        a[jMax] = 0.;b[jMax] = 1.;c[jMax] = 0.;
+        d[jMax] = 0.;
+        // solve with SOR method
+        int sor;
+        for(sor=0;sor<iterMax;sor++)
+        {
+            double error=0.;
+            // implement sor in here
+            {
+                double y = (d[0] - c[0]*vNew[1])/b[0];
+		y = std::max(y,X-S[0]);
+                vNew[0] = vNew[0] + omega*(y-vNew[0]); 
+            }
+            for(int j=1;j<jMax;j++)
+            {
+                double y = (d[j] - a[j]*vNew[j-1] - c[j]*vNew[j+1])/b[j];
+		y = std::max(y,X-S[j]);
+                vNew[j] = vNew[j] + omega*(y-vNew[j]); 
+            }
+            {
+                double y = (d[jMax] - a[jMax]*vNew[jMax-1])/b[jMax];
+		y = std::max(y,X-S[jMax]);
+                vNew[jMax] = vNew[jMax] + omega*(y-vNew[jMax]); 
+            }
+            // calculate residual norm ||r|| as sum of absolute values
+            error += fabs(d[0] - b[0]*vNew[0] - c[0]*vNew[1]);
+            for(int j=1;j<jMax;j++)
+                error += fabs(d[j] - a[j]*vNew[j-1] - b[j]*vNew[j] - c[j]*vNew[j+1]);
+            error += fabs(d[jMax] - a[jMax]*vNew[jMax-1] - b[jMax]*vNew[jMax]);
+            // make an exit condition when solution found
+            if(error<tol)
+                break;
+        }
+        if(sor>=iterMax)
+        {
+            std::cout << " Error NOT converging within required iterations\n";
+            std::cout.flush();
+            throw;
+        }
+        // set old=new
+        vOld=vNew;
+    }
+    int jStar=S0/dS;
+    double sum=0.;
+    sum+=(S0 - S[jStar])/dS * vNew[jStar+1];
+    sum+=(S[jStar+1] - S0)/dS * vNew[jStar];
+    return sum;
+}
+
 
 double normalDistribution(double x)
 {
