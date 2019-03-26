ec8f47d41907f40462742f5d8e2f5206772fb12d
Paul Johnson <p.johno@gmail.com>
2019-03-26 23:26:48 +0000

    update tridag example to contain thomas solver as well

--- main/math60082_tridag_method.cpp
+++ main/math60082_tridag_method.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include "math60082_sor.hpp"
-#include "math60082_tridag.hpp"
 using namespace std;
+
+/* 
+ *    ON INPUT:
+ *    a, b and c -- are the diagonals of the matrix
+ *    rhs        -- is the right hand side
+ *    ON OUTPUT:
+ *    a, b, c             -- unchanged
+ *    rhs                 -- solution to Ax=b
+ */
+void thomasSolve(const std::vector<double> &a,const std::vector<double> &b_,const std::vector<double> &c,std::vector<double> &rhs)
+{
+    int n=a.size();
+    std::vector<double> b(n);
+    // initial first value of b
+    b[0]=b_[0];
+    for(int j=1;j<n;j++)
+    {
+        b[j]=b_[j]-c[j-1]*a[j]/b[j-1];  
+        rhs[j]=rhs[j]-rhs[j-1]*a[j]/b[j-1];
+    }
+    // calculate solution
+    rhs[n-1]=rhs[n-1]/b[n-1];
+    for(int j=n-2;j>=0;j--)
+        rhs[j]=(rhs[j]-c[j]*rhs[j+1])/b[j];
+}
 
 int main()
 {
@@ -14,32 +37,13 @@ int main()
     // declare vector for the rhs v and solution x 
     vector<double> rhs = {1.,0.25,0.5,0.};
     vector<double> x;
-    // first try
-    // reset initial guess for x
-    x = {0.,0.,0.,0.};
-    int sor;
-    // iterMax=50, tol=1.e-6, omega = 1
-    MATH60082::sorSolve(a,b,c,rhs,x,50,1.e-6,1.,sor);
-    cout << "Converged after " << sor+1 << " iterations.\n";
-    for(int varyOmega=5;varyOmega<=20;varyOmega++)
-    {
-        // reset initial guess for x
-        x = {0.,0.,0.,0.};
-        // this time omega = 1.1
-        double omega = varyOmega/10.;
-        MATH60082::sorSolve(a,b,c,rhs,x,5000,1.e-6,omega,sor);
-        cout << "Try with omega = " << omega << " we get x = { ";
-        for(auto xi : x)cout << xi << " ";
-        cout << "} after " << sor+1 << " iterations.\n";
-    }
-    
-    // reset x with the rhs
+    // first reset x with the rhs
     x = rhs;
     // on exit x will contain the solution
-    MATH60082::thomasSolve(a,b,c,x);
-    cout << "Try with thomas solver we get x = { ";
-        for(auto xi : x)cout << xi << " ";
-        cout << "} " << endl;
-
+    thomasSolve(a,b,c,x);
+    cout << "Solve with thomas algorithm we get x = { ";
+    for(auto xi : x)cout << xi << " ";
+    cout << "} " << endl;
+    
 }
 
