2d19936890229404c83a29d77678434f2eae624f
Paul Johnson <p.johno@gmail.com>
2021-05-18 12:27:38 +0100

    Add markup table output

---
+++ include/doxygen_table.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+namespace MATH60082{
+  
+  template<class T=std::string>
+  void addColumnEntry(const T& t=std::string("--------------"))
+  {
+    static const int columnWidth=14;
+    cout << "|" << setw(columnWidth)  << t;
+  }
+  void endRow()
+  {
+    cout << "|" <<endl;
+  }
+  void emptyTableRow(unsigned int n)
+  {
+    for(unsigned int i=0;i<n;i++)
+    {
+      addColumnEntry();
+    }
+    cout << "|" <<endl;
+  }
+  
+  template<typename T>
+  void tableRow(T v) {
+    addColumnEntry(v);
+    endRow();
+  }
+  template<typename T, typename... Args>
+  void tableRow(T first, Args... args) {
+    addColumnEntry(first);
+    tableRow(args...);
+  }
+  
+}
