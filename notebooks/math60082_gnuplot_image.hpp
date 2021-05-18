fbc329e1108a764942192182423e622f71c76bb9
Paul Johnson <p.johno@gmail.com>
2021-05-18 11:24:32 +0100

    Update to latest cling

--- notebooks/math60082_gnuplot_image.hpp
+++ notebooks/math60082_gnuplot_image.hpp
@@ -1,14 +1,15 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include <iosfwd>
+#include <sstream>
+
 #include <boost/archive/iterators/binary_from_base64.hpp>
 #include <boost/archive/iterators/base64_from_binary.hpp>
 #include <boost/archive/iterators/transform_width.hpp>
 #include <boost/algorithm/string.hpp>
-#include <vector>
-#include "boost/process.hpp"
-#include <iostream>
-#include <iosfwd>
+#include <boost/process.hpp>
 #include <boost/chrono.hpp>
-#include <sstream>
 #include "xeus/xjson.hpp"
 
 namespace GPLT{
