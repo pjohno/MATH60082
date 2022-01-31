20c677238fa072da2c2c5816c39518b0fad150a7
pjohno <p.johno@gmail.com>
2022-01-31 11:37:13 +0000

    Add plot loader

---
+++ notebooks/math60082_lab_plot_loader.hpp
@@ -0,0 +1,10 @@
+#include "math60082_cling_libs_loader.hpp"
+
+#include "math60082_markupTable.hpp"
+#include "math60082_gnuplot.hpp"
+#include <vector>
+#include <iostream>
+
+#include "math60082_gnuplot_image.hpp"
+
+using namespace MATH60082;
