ac60a1bc9ffe4954b78af6eca3c6d314d04ac3da
pjohno <p.johno@gmail.com>
2022-09-12 13:29:32 +0100

    Update environment to fix problems with gcc version against mybinder version

---
+++ include/math60082_testProject.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/** \file math60082_testProject.hpp
+\brief Commented file
+*/
+namespace MATH60082
+{
+  /*! \fn int testProject(void)
+     \brief Output to screen test
+  */
+  int testProject(void);
+}
+
---
+++ src/math60082_testProject.cpp
@@ -0,0 +1,15 @@
+#include "math60082_testProject.hpp"
+#include <iostream>
+using namespace std;
+
+namespace MATH60082
+{
+  int testProject(void)
+  {
+    int exit_status = 0;
+    cout << " C++ Library :: MATH60082 " << endl;
+    return exit_status;
+  }
+}
+
+
